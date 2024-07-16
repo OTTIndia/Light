5cd97c396d3576fabca389641960baa73a49367a
xdumaster1 <145930645+xdumaster1@users.noreply.github.com>
2024-07-16 21:27:09 +0530

    Update app_priv.h

--- examples/main/app_priv.h
+++ examples/main/app_priv.h
@@ -15,30 +15,11 @@
 #include <esp_matter.h>
 #include <driver/gpio.h>
 
-#if CHIP_DEVICE_CONFIG_ENABLE_THREAD
-#include "esp_openthread_types.h"
-#endif
-
-/** GPIO definition for the LED */
-#define LED_GPIO_PIN GPIO_NUM_2
-
-/** Standard max values (used for remapping attributes) */
-#define STANDARD_BRIGHTNESS 100
-#define STANDARD_HUE 360
-#define STANDARD_SATURATION 100
-#define STANDARD_TEMPERATURE_FACTOR 1000000
-
-/** Matter max values (used for remapping attributes) */
-#define MATTER_BRIGHTNESS 254
-#define MATTER_HUE 254
-#define MATTER_SATURATION 254
-#define MATTER_TEMPERATURE_FACTOR 1000000
+/** GPIO definitions for the LED */
+#define LED_GPIO GPIO_NUM_2
 
 /** Default attribute values used during initialization */
 #define DEFAULT_POWER true
-#define DEFAULT_BRIGHTNESS 64
-#define DEFAULT_HUE 128
-#define DEFAULT_SATURATION 254
 
 typedef void *app_driver_handle_t;
 
@@ -87,22 +68,5 @@ esp_err_t app_driver_attribute_update(app_driver_handle_t driver_handle, uint16_
  * @return error in case of failure.
  */
 esp_err_t app_driver_light_set_defaults(uint16_t endpoint_id);
-
-#if CHIP_DEVICE_CONFIG_ENABLE_THREAD
-#define ESP_OPENTHREAD_DEFAULT_RADIO_CONFIG()                                           \
-    {                                                                                   \
-        .radio_mode = RADIO_MODE_NATIVE,                                                \
-    }
-
-#define ESP_OPENTHREAD_DEFAULT_HOST_CONFIG()                                            \
-    {                                                                                   \
-        .host_connection_mode = HOST_CONNECTION_MODE_NONE,                              \
-    }
-
-#define ESP_OPENTHREAD_DEFAULT_PORT_CONFIG()                                            \
-    {                                                                                   \
-        .storage_partition_name = "nvs", .netif_queue_size = 10, .task_queue_size = 10, \
-    }
-#endif
 
 #endif // APP_PRIV_H
