19398cc4133c624a68c8ee1f4fb31dbc4f79830b
xdumaster1 <145930645+xdumaster1@users.noreply.github.com>
2024-07-21 14:54:07 +0530

    Update app_driver.cpp

--- examples/main/app_driver.cpp
+++ examples/main/app_driver.cpp
@@ -10,8 +10,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <device.h>
 #include <esp_matter.h>
+#include "bsp/esp-bsp.h"
+
 #include <app_priv.h>
 
 using namespace chip::app::Clusters;
@@ -20,59 +21,96 @@ using namespace esp_matter;
 static const char *TAG = "app_driver";
 extern uint16_t light_endpoint_id;
 
-/* Set the on/off state of the light */
-static esp_err_t app_driver_light_set_on_off(esp_matter_attr_val_t *val)
+/* Set the power state of the light */
+static esp_err_t app_driver_light_set_power(led_indicator_handle_t handle, esp_matter_attr_val_t *val)
 {
-    ESP_LOGI(TAG, "Changing the GPIO LED!");
-
-    gpio_set_level(LED_GPIO, val->val.b ? 1 : 0);
-
+#if CONFIG_BSP_LEDS_NUM > 0
+    esp_err_t err = ESP_OK;
+    if (val->val.b) {
+        err = led_indicator_start(handle, BSP_LED_ON);
+    } else {
+        err = led_indicator_start(handle, BSP_LED_OFF);
+    }
+    return err;
+#else
+    ESP_LOGI(TAG, "LED set power: %d", val->val.b);
     return ESP_OK;
+#endif
+}
+
+/* Toggle button callback */
+static void app_driver_button_toggle_cb(void *arg, void *data)
+{
+    ESP_LOGI(TAG, "Toggle button pressed");
+    uint16_t endpoint_id = light_endpoint_id;
+    uint32_t cluster_id = OnOff::Id;
+    uint32_t attribute_id = OnOff::Attributes::OnOff::Id;
+
+    node_t *node = node::get();
+    endpoint_t *endpoint = endpoint::get(node, endpoint_id);
+    cluster_t *cluster = cluster::get(endpoint, cluster_id);
+    attribute_t *attribute = attribute::get(cluster, attribute_id);
+
+    esp_matter_attr_val_t val = esp_matter_invalid(NULL);
+    attribute::get_val(attribute, &val);
+    val.val.b = !val.val.b;
+    attribute::update(endpoint_id, cluster_id, attribute_id, &val);
 }
 
 esp_err_t app_driver_attribute_update(app_driver_handle_t driver_handle, uint16_t endpoint_id, uint32_t cluster_id,
                                       uint32_t attribute_id, esp_matter_attr_val_t *val)
 {
     esp_err_t err = ESP_OK;
-
-    /* Handle the On/Off cluster */
-    if (endpoint_id == light_endpoint_id && cluster_id == OnOff::Id) {
-        if (attribute_id == OnOff::Attributes::OnOff::Id) {
-            err = app_driver_light_set_on_off(val);
+    if (endpoint_id == light_endpoint_id) {
+        led_indicator_handle_t handle = (led_indicator_handle_t)driver_handle;
+        if (cluster_id == OnOff::Id) {
+            if (attribute_id == OnOff::Attributes::OnOff::Id) {
+                err = app_driver_light_set_power(handle, val);
+            }
         }
     }
-
     return err;
-}
-
-app_driver_handle_t app_driver_light_init()
-{
-    /* Initialize GPIO for LED */
-    gpio_config_t io_conf;
-    io_conf.intr_type = GPIO_INTR_DISABLE;
-    io_conf.mode = GPIO_MODE_OUTPUT;
-    io_conf.pin_bit_mask = (1ULL << LED_GPIO);
-    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
-    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
-    gpio_config(&io_conf);
-
-    return NULL;
 }
 
 esp_err_t app_driver_light_set_defaults(uint16_t endpoint_id)
 {
     esp_err_t err = ESP_OK;
+    void *priv_data = endpoint::get_priv_data(endpoint_id);
+    led_indicator_handle_t handle = (led_indicator_handle_t)priv_data;
     node_t *node = node::get();
     endpoint_t *endpoint = endpoint::get(node, endpoint_id);
     cluster_t *cluster = NULL;
     attribute_t *attribute = NULL;
     esp_matter_attr_val_t val = esp_matter_invalid(NULL);
 
-    /* Set the default power state */
+    /* Setting power */
     cluster = cluster::get(endpoint, OnOff::Id);
     attribute = attribute::get(cluster, OnOff::Attributes::OnOff::Id);
     attribute::get_val(attribute, &val);
-    err |= app_driver_light_set_on_off(&val);
+    err |= app_driver_light_set_power(handle, &val);
 
     return err;
+}
+
+app_driver_handle_t app_driver_light_init()
+{
+#if CONFIG_BSP_LEDS_NUM > 0
+    /* Initialize led */
+    led_indicator_handle_t leds[CONFIG_BSP_LEDS_NUM];
+    ESP_ERROR_CHECK(bsp_led_indicator_create(leds, NULL, CONFIG_BSP_LEDS_NUM));
+    
+    return (app_driver_handle_t)leds[0];
+#else
+    return NULL;
+#endif
+}
+
+app_driver_handle_t app_driver_button_init()
+{
+    /* Initialize button */
+    button_handle_t btns[BSP_BUTTON_NUM];
+    ESP_ERROR_CHECK(bsp_iot_button_create(btns, NULL, BSP_BUTTON_NUM));
+    ESP_ERROR_CHECK(iot_button_register_cb(btns[0], BUTTON_PRESS_DOWN, app_driver_button_toggle_cb, NULL));
+    
+    return (app_driver_handle_t)btns[0];
 }
